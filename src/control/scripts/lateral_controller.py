69bc9aa2de74d64ef0ea3a14bd94ea399f084d7a
jjo4342 <jjo4342@gmail.com>
2024-05-15 18:58:01 +0900

    [feat] Add new class to avoide obstacles

--- src/control/scripts/lateral_controller.py
+++ src/control/scripts/lateral_controller.py
@@ -10,7 +10,7 @@ from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry, Path
 from morai_msgs.msg import EgoVehicleStatus
 
-class pure_pursuit:
+class pure_pursuit_no_npc:
     def __init__(self):
 
 
@@ -27,7 +27,7 @@ class pure_pursuit:
         self.forward_point = Point()
         self.current_postion = Point()
 
-        self.vehicle_length = 4.355  # Hyeondai Ioniq (hev)
+        self.vehicle_length = 5.205  # Hyeondai Ioniq (hev)
         self.lfd = 10
         self.min_lfd = 10
         self.max_lfd = 20  # default 30
@@ -100,7 +100,100 @@ class pure_pursuit:
 
         theta = atan2(local_path_point[1], local_path_point[0])
         steering = atan2((2 * self.vehicle_length * sin(theta)), self.lfd)
-        
+        # print(steering)
+        return steering
+
+class pure_pursuit:
+    def __init__(self):
+
+
+        # rospy.init_node('pure_pursuit', anonymous=True)
+
+        rospy.Subscriber("/lattice_path", Path, self.path_callback)
+        rospy.Subscriber("/odom", Odometry, self.odom_callback)
+        rospy.Subscriber("/Ego_topic", EgoVehicleStatus, self.status_callback)
+
+        self.is_path = False
+        self.is_odom = False
+        self.is_status = False
+
+        self.forward_point = Point()
+        self.current_postion = Point()
+
+        self.vehicle_length = 5.205  # Hyeondai Ioniq (hev)
+        self.lfd = 10
+        self.min_lfd = 10
+        self.max_lfd = 20  # default 30
+        self.lfd_gain = 3  # default 0.78
+
+    def path_callback(self, msg):
+        self.is_path = True
+        self.path = msg
+
+    def odom_callback(self, msg):
+        self.is_odom = True
+        odom_quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z,
+                           msg.pose.pose.orientation.w)
+        _, _, self.vehicle_yaw = euler_from_quaternion(odom_quaternion)
+        self.current_postion.x = msg.pose.pose.position.x
+        self.current_postion.y = msg.pose.pose.position.y
+
+    def status_callback(self, msg):  ## Vehicle Status Subscriber
+        self.is_status = True
+        self.status_msg = msg
+        self.global_path = msg
+        self.is_global_path = True
+
+    def get_current_waypoint(self, ego_status, global_path):
+        min_dist = float('inf')
+        currnet_waypoint = -1
+        for i, pose in enumerate(global_path.poses):
+            dx = ego_status.position.x - pose.pose.position.x
+            dy = ego_status.position.y - pose.pose.position.y
+
+            dist = sqrt(pow(dx, 2) + pow(dy, 2))
+            if min_dist > dist:
+                min_dist = dist
+                currnet_waypoint = i
+        return currnet_waypoint
+
+    def calc_pure_pursuit(self, ):
+
+        self.lfd = (self.status_msg.velocity.x) * self.lfd_gain
+
+        if self.lfd < self.min_lfd:
+            self.lfd = self.min_lfd
+        elif self.lfd > self.max_lfd:
+            self.lfd = self.max_lfd
+
+        vehicle_position = self.current_postion
+        self.is_look_forward_point = False
+
+        translation = [vehicle_position.x, vehicle_position.y]
+
+        trans_matrix = np.array([
+            [cos(self.vehicle_yaw), -sin(self.vehicle_yaw), translation[0]],
+            [sin(self.vehicle_yaw), cos(self.vehicle_yaw), translation[1]],
+            [0, 0, 1]])
+
+        det_trans_matrix = np.linalg.inv(trans_matrix)
+
+        for num, i in enumerate(self.path.poses):
+            path_point = i.pose.position
+
+            global_path_point = [path_point.x, path_point.y, 1]
+            local_path_point = det_trans_matrix.dot(global_path_point)
+
+            if local_path_point[0] > 0:
+                dis = sqrt(pow(local_path_point[0], 2) + pow(local_path_point[1], 2))
+                if dis >= self.lfd:
+                    self.forward_point = path_point
+                    self.is_look_forward_point = True
+                    break
+
+        theta = atan2(local_path_point[1], local_path_point[0])
+        steering = atan2((2 * self.vehicle_length * sin(theta)), self.lfd)
+        # print(steering)
         return steering
 
 
@@ -130,14 +223,8 @@ class stanley :
         self.forward_point = Point()
         self.current_position = Point()
 
-        self.vehicle_length = 5.155
-        self.lfd = 10
-        if self.vehicle_length is None or self.lfd is None:
-            print("you need to change values at line 57~58 ,  self.vegicle_length , lfd")
-            exit()
-        # self.min_lfd = 10
-        # self.max_lfd = 30
-        # self.lfd_gain = 0.78
+        self.vehicle_length = 5.205
+
         self.target_velocity = 40
 
 
@@ -176,7 +263,7 @@ class stanley :
 
 
     def calc_stanley_control(self):
-            k = 1  # Stanley gain
+            k = 1.0  # Stanley gain
             min_distance = float('inf')
             nearest_idx = -0.0
 
@@ -196,19 +283,18 @@ class stanley :
             cos_yaw = cos(self.vehicle_yaw)
             sin_yaw = sin(self.vehicle_yaw)
             cross_track_error = dx * sin_yaw - dy * cos_yaw
-                        
+
             # heading error
-            dx = self.global_path.poses[nearest_idx + 5].pose.position.x - self.global_path.poses[nearest_idx].pose.position.x
-            dy = self.global_path.poses[nearest_idx + 5].pose.position.y - self.global_path.poses[nearest_idx].pose.position.y
-            # heading_error = atan2(dy, dx) - self.vehicle_yaw
-            path_heading = atan2(dy, dx)
-            heading_error = path_heading - self.vehicle_yaw
-            # rospy.loginfo("heading_error: %s", heading_error)
-            # rospy.loginfo("cross_track_error: %s", cross_track_error)
-            
+            dx = self.global_path.poses[nearest_idx + 3].pose.position.x - self.global_path.poses[nearest_idx].pose.position.x
+            dy = self.global_path.poses[nearest_idx + 3].pose.position.y - self.global_path.poses[nearest_idx].pose.position.y
+            heading_error = (atan2(dy, dx) - self.vehicle_yaw) / pi
+
+            print("heading_error: ", heading_error)
+            print("CTE: ", atan2(k * cross_track_error, self.target_velocity))
+
             # Calculate steering using Stanley method
-            steering = heading_error + atan2(k * cross_track_error, self.target_velocity)
-            rospy.loginfo("steering: %s", steering)
+            steering = (heading_error + atan2(k * cross_track_error, self.target_velocity))
+            print("steering: ", steering, "\n")
 
             return steering
 
