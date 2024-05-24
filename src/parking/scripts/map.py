e39544337c8d7832e6400177c8996fbc2d88eea8
Dahyun Noh <dk3146@naver.com>
2024-05-24 15:48:20 +0900

    [feat] added map

--- src/parking/scripts/map.py
+++ src/parking/scripts/map.py
@@ -2,36 +2,55 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import obstacle as obstacle
+from scenarioLoader import ScenarioLoader
+
+def pify(alpha):
+    v = np.fmod(alpha, 2*np.pi)
+    if (v < - np.pi):
+        v += 2 * np.pi
+    else:
+        v -= 2 * np.pi
+    return v
+
+def degrees_to_radians(degrees):
+    return degrees * (np.pi / 180.0)
 
 def map():
     # Start, goal : [x, y, theta]
-    start = [10.0, 0.0, np.pi * 1.0]
-    goal = [10.0, 10.0, 0.0]
-    # Searching space : [min_x, max_x, min_y, max_y]
-    space = [-2.0, 15.0, -2.0, 15.0]
-    # Obstacle : (x, y, radius)
-    r = 0.5
-    obstacle_list = [[3, 5, r],
-                     [3, 6, r],
-                     [3, 7, r],
-                     [3, 8, r],
-                     [3, 9, r],
-                     [3, 10, r],
-                     [4, 5, r],
-                     [5, 5, r],
-                     [6, 5, r],
-                     [7, 5, r],
-                     [8, 5, r],
-                     [9, 5, r],
-                     [10, 5, r],
-                     [11, 5, r],
-                     [12, 5, r],
-                     [13, 5, r],
-                     [14, 5, r],
-                     [15, 5, r]]
-    #obstacle_list = [[10, 6, 1]]
+    start = [5.5, 1068.8048, degrees_to_radians(-35.765)]
+    goal = [5.25, 1021.73, degrees_to_radians(-120.769)]
 
-    return start, goal, obstacle_list, space
+    # Searching space : [min_x, max_x, min_y, max_y]
+    space = [-18.19, 43.6, 1004.84, 1076.51]
+
+    # Obstacle : (x, y, radius)
+    obstacleList =[]
+    car_size = [4.96, 2.069] # 4.96 , 2.069
+    obstacleId = {
+        'car' : 40100019,
+        'corn' : 40100003
+    }
+
+    scenario = ScenarioLoader()
+    objectList = scenario.getObject()
+
+    for object in objectList :
+        DataId = object['DataID']
+        DataPos = object['pos']
+        DataYaw = pify(degrees_to_radians(float(object['rot']['yaw'])))
+
+        DataX = DataPos['x']
+        DataY = DataPos['y']
+
+        if DataId == obstacleId['car'] :
+            obs = obstacle.RectangleObstacle(DataX,DataY,car_size[0],car_size[1],DataYaw)
+            obstacleList.append(obs)
+        elif DataId == obstacleId['corn'] :
+            obs = obstacle.Obstacle(DataX,DataY,0.6)
+            obstacleList.append(obs)
+
+    return start, goal, obstacleList, space
 
 if __name__ == "__main__":
     theta_plot = np.linspace(0,1,101) * np.pi * 2
@@ -39,12 +58,11 @@ if __name__ == "__main__":
     plt.figure(figsize=(8,8))
     plt.plot(start[0], start[1], 'bs',  markersize=7)
     plt.text(start[0], start[1]+0.5, 'start', fontsize=12)
+    
     plt.plot(goal[0], goal[1], 'rs',  markersize=7)
     plt.text(goal[0], goal[1]+0.5, 'goal', fontsize=12)
-    for i in range(len(obstacle_list)):
-        x_obstacle = obstacle_list[i][0] + obstacle_list[i][2] * np.cos(theta_plot)
-        y_obstacle = obstacle_list[i][1] + obstacle_list[i][2] * np.sin(theta_plot)
-        plt.plot(x_obstacle, y_obstacle,'k-')
+    for obs in obstacle_list :
+        obs.plot()
     plt.axis(space)
-    plt.grid(True)
+    # plt.grid(True)
     plt.show()
