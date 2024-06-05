7aa96fe7f5ab89640ccba41eed7b885479bda2be
henricus <henricus0973@korea.ac.kr>
2024-06-05 23:54:16 +0900

    Added Publishing Node Topic using message.

--- src/prediction/prediction/scripts/bayseian.py
+++ src/prediction/prediction/scripts/bayseian.py
@@ -13,18 +13,20 @@ from nav_msgs.msg import Odometry, Path
 from morai_msgs.msg import EgoVehicleStatus, ObjectStatus, ObjectStatusList, EventInfo
 from morai_msgs.srv import MoraiEventCmdSrv
 from std_msgs.msg import Int32
-from prediction.msg import TrackedPoint, PredictedObjectPath, PredictedObjectPathList, TrackedObjectPose, TrackedObjectPoseList
+from prediction.msg import TrackedPoint, PredictedObjectPath, PredictedObjectPathList, TrackedObjectPose, TrackedObjectPoseList, PredictedHMM
 import scipy
 from scipy.stats import norm, multivariate_normal
 from filter import Extended_KalmanFilter, IMM_filter
 import numpy as np
 from hmmlearn import hmm
 import json
+from std_msgs.msg import String
 # from frame_transform import *
-
 
 from model import CTRA, CA
 
+
+##################          HMM MODEL           ###############
 class VehicleBehaviorHMM:
     def __init__(self, lane_width, v_max):
         self.lane_width = lane_width
@@ -34,20 +36,27 @@ class VehicleBehaviorHMM:
         self.states = ["Lane Keeping", "Right Lane Change", "Left Lane Change"]
         self.n_states = len(self.states)
 
+
         # 초기 상태 확률
         self.start_probabilities = np.array([0.6, 0.2, 0.2])
 
         # 상태 전이 확률 행렬
         self.transition_matrix = np.array([
-            [0.8, 0.1, 0.1],
-            [0.2, 0.6, 0.2],
-            [0.2, 0.2, 0.6]
+            [0.7, 0.15, 0.15], # Lane Keeping -> Others
+            [0.6, 0.2,  0.2],  # Right Lane Change -> Others
+            [0.6, 0.2,  0.2]   # Left Lane Changes -> Others
         ])
 
         # HMM 모델 생성
         self.model = hmm.GaussianHMM(n_components=self.n_states, covariance_type="full", init_params="")
         self.model.startprob_ = self.start_probabilities
         self.model.transmat_ = self.transition_matrix
+
+        # Covariance
+        self.model.covars_ = np.full((self.n_states, 1,1), 0.01)
+
+        # MEANS
+        self.model.means_ = np.zeros((self.n_states, 1))
 
     def calculate_emission_probabilities(self, d_lat, v):
         # Lane Change Profile Probability
@@ -63,68 +72,83 @@ class VehicleBehaviorHMM:
             p_lcv(d_lat, v),  # Right Lane Change
             p_lcv_minus(d_lat, v)  # Left Lane Change
         ])
+        print("Emission matrix", emission_probabilities)
 
         return emission_probabilities
 
     def fit(self, observations):
-        # 관측값 (d_lat, v) 시퀀스에서 방출 확률 계산
-        emission_probs = np.array([self.calculate_emission_probabilities(d_lat, v) for d_lat, v in observations])
+        try:
+            # 관측값 (d_lat, v) 시퀀스에서 방출 확률 계산
+            emission_probs = np.array([self.calculate_emission_probabilities(d_lat, v) for d_lat, v in observations])
+            if not emission_probs.size:
+                raise ValueError("Empty emission probabilities")
+            # HMM의 means와 covariances 설정 (방출 확률을 기준으로 함)
+            self.model.means_ = np.mean(emission_probs, axis=0).reshape(-1, 1)
+            #self.model.covars_ = np.var(emission_probs, axis=0).reshape(-1, 1, 1)
+            self.model.covars_ = np.array([np.diag(np.var(emission_probs, axis=0) + 0.0001)])
+            # 모델 적합
+            self.model.fit(emission_probs.reshape(-1,1))
 
-        # HMM의 means와 covariances 설정 (방출 확률을 기준으로 함)
-        self.model.means_ = np.mean(emission_probs, axis=0).reshape(-1, 1)
-        self.model.covars_ = np.var(emission_probs, axis=0).reshape(-1, 1, 1)
-
-        # 모델 적합
-        self.model.fit(emission_probs)
+        except Exception as e:
+            rospy.logerr(f"Failed to fit model: {e}")
 
     def predict(self, observations):
-        # 관측값 (d_lat, v) 시퀀스에서 방출 확률 계산
-        emission_probs = np.array([self.calculate_emission_probabilities(d_lat, v) for d_lat, v in observations])
+        try:
+            if observations and all(isinstance(obs, tuple) and len(obs) ==2 for obs in observations):
+                # 관측값 (d_lat, v) 시퀀스에서 방출 확률 계산
+                emission_probs = np.array([self.calculate_emission_probabilities(d_lat, v) for d_lat, v in observations])
+                if emission_probs.size == 0:
+                    raise ValueError("No valid observation")
+                # Viterbi 알고리즘을 이용하여 가장 가능성 높은 상태 시퀀스를 계산
+                logprob, states = self.model.decode(emission_probs.reshape(-1,1), algorithm="viterbi")
 
-        # Viterbi 알고리즘을 이용하여 가장 가능성 높은 상태 시퀀스를 계산
-        logprob, state_sequence = self.model.decode(emission_probs, algorithm="viterbi")
+                return logprob, states
+                #return state_sequence
+            else:
+                raise ValueError("Invalid observation data")
 
-        return state_sequence
+        except Exception as e:
+            rospy.logerr(f"Prediction failed: {e}")
+            return None, []
 
+################      Vehicle Tracker     ##############
 class VehicleTracker():
     def __init__(self,dt=0.1, T=1):
+        '''
+            PredictedHMM.msg
+            Header header
+            int32 unique_id
+            String maneuver
+            float64 probability
+        '''
         rospy.init_node('VehicleTracker', anonymous=True)
         rospy.Subscriber("/Object_topic", ObjectStatusList, self.object_info_callback)
         rospy.Subscriber("/global_path", Path, self.global_path_callback)
+        self.state_prediction = rospy.Publisher("/Object_topic/hmm_prediction", PredictedHMM, queue_size= 10)
 
         self.dt=dt
         self.T=T
-        self.v_max= 80.0 # v_max 정하셈... m/s
+        self.v_max= 60.0 # Maximum Velocity :: Parameter Tunning
         self.rate=rospy.Rate(30)
         self.lane_width=3.521 # initial value
+
+        ## Message Info
         self.is_object = False
         self.is_global_path = False
         self.object_data = None
+
         self.previous_heading = {}
         self.previous_time = rospy.Time.now()
 
+        # Get Global Path
         self.global_path_msg = Path()
         self.global_path_msg.header.frame_id = 'map'
         self.x = 0
         self.y = 0
         self.yaw = 0
+        # Managing Object
+        self.hmm_models = {}
 
-
-
-        # # Parameter, 인공지능 딥러닝 모델 만들어서 학습시켜야 함... 노가다 뛸 각오 하셈
-        # self.states = ["Lane Keeping", "Right Lane Change", "Left Lane Change"]
-        # self.mu=np.array([0.9, 0.05, 0.05]) # Initial Transition Matrix
-        # self.action_transition_matrix=np.array([
-        #                             [0.8, 0.1,  0.1], # 1번째 state LK
-        #                             [0.6, 0.01, 0.39], # 2번째 state Right change
-        #                             [0.6, 0.39, 0.01] # 3번째 state Left Change
-        #                             ])
-   
-        # # self.velocity_observations = ["low speed", "high speed"]
-        # # self.distance_observations = []
-        # self.model = hmm.GaussianHMM(n_components=3, covariance_type= " full")
-        # self.model.startprob_=self.mu
-        # self.model.transmat_ = self.action_transition_matrix
 
     ##########   CALLBACK Function   ########
     # Object Callback Function, Get the object info
@@ -142,7 +166,7 @@ class VehicleTracker():
     def data_preprocessing(self, obstacle, delta_time):
         obj_id = obstacle.unique_id
 
-        # Calculating Velocity
+        # Calculating Velocity -> You have to Use Longitudinal Velocity
         v = sqrt(pow(obstacle.velocity.x, 2) + pow(obstacle.velocity.y, 2))
 
         # Calculating ACC
@@ -177,18 +201,24 @@ class VehicleTracker():
         #P(M|C)
         p_lk=norm.pdf(d_lat, 0, 0.4) # lk 확률
         p_lc=norm.pdf(d_lat, lane_width/2, 0.4) # lc 확률
+        print("Possibility of p_lk ", p_lkv)
+        print("Possibility of left lane change" , p_lcv)
+        print("Possibility of right lane change", p_lcv_minus)
 
         return p_lcv, p_lcv_minus, p_lkv, p_lk, p_lc
 
     ## MAIN PROGRAM ##
     def run(self):
         while not rospy.is_shutdown():
+            predicted_list = []
             self.hmm_model = VehicleBehaviorHMM(self.lane_width, self.v_max)
             if self.is_object == True & self.is_global_path == True:
-                print("BB")
-                file_path = '/home/henricus/final_project/src/prediction/src/added_sensor_network.json'
+                print("Received Global Path for Prediction")
                 current_time = rospy.Time.now()
                 delta_time = (current_time - self.previous_time).to_sec()
+                self.previous_time = current_time
+                observations = {}
+
                 #TODO(4) : Get Frenet Frame coordinate info of the other vehicles based on Ego Vehicle
                 for obstacle in self.object_data.npc_list: # Get by callback function
                     obj_id = obstacle.unique_id
@@ -200,24 +230,42 @@ class VehicleTracker():
                     mapx = [pose.pose.position.x for pose in self.global_path_msg.poses]
                     mapy = [pose.pose.position.y for pose in self.global_path_msg.poses]
 
-                    s,d = get_frenet(x, y, mapx, mapy=mapy)
+                    s,d = get_frenet(x, y, mapx, mapy)
 
-                    # Normalize d value
+                    # Normalize d value (frenet coordinate value of each object(vehicle))
                     if d>0:
                         d_int = d // self.lane_width
                         d = d - d_int * self.lane_width
+
                     if d<0:
                         d_int = d// self.lane_width +1
                         d = d - d_int * self.lane_width
+
                     # Probability, b_ij, #Emission Possibility
                     # Observation Sequence (d_lat, v)
-                    observation = np.array([d, data[4]])
-                    self.hmm_model.fit(observation)
-                    state_sequence = self.hmm_model.predict(observation)
-                    predicted_states = [self.hmm_model.states[state] for state in state_sequence]
-                    print("Predicted States:")
-                    print(predicted_states)
 
+                    observation = (d, data[3])
+                    print("observation:: ", observation)
+                    #TODO(5) : HMM Model and Publish the TOPIC
+                    if obj_id not in self.hmm_models:
+                        self.hmm_models[obj_id] = VehicleBehaviorHMM(self.lane_width, self.v_max)
+                    # Observations dictionary에 observation값 추가. time stamp관리?
+                    # observations.append(observation)
+                    #self.hmm_models[obj_id].fit([observations])
+                    logprob, state_sequence = self.hmm_models[obj_id].predict([observation])
+
+                    if state_sequence.size>0:
+                        predicted_state = self.hmm_models[obj_id].states[state_sequence[0]]
+                        probability = np.exp(logprob)
+                        predicted_object = PredictedHMM(unique_id = obj_id, maneuver=predicted_state, probability=probability)
+                        predicted_list.append(predicted_object)
+                        # prediction_msg = f"Object ID: {obj_id}, Predicted State: {predicted_state}, Probability: {probability:.4f}"
+                        print("prediction message", predicted_list)
+                        # Publish ROS TOPIC
+                        self.state_prediction.publish(predicted_object)
+                    else:
+                        rospy.logerr("ERROR:: Not Effective Data Type or State Sequence")
+                self.rate.sleep()
 
     def callback_result(self, data):
         s_value, d_value= self.frenet_frame(data)
@@ -225,6 +273,7 @@ class VehicleTracker():
             veh_data=np.array(self.vehicles[i][self.time-10:self.time+1])
 
 
+###################         UTILS FUNCTION            ###############
 def next_waypoint(x, y, mapx, mapy):
     closest_wp = 0
     closest_wp = get_closest_waypoints(x, y, mapx, mapy)
@@ -234,7 +283,6 @@ def next_waypoint(x, y, mapx, mapy):
 
     # if closest_wp >= len(mapx) or closest_wp < 0:
     #     closest_wp = 0
-
 
     map_x = mapx[closest_wp]
     map_y = mapy[closest_wp]
@@ -297,7 +345,7 @@ def get_frenet(x, y, mapx, mapy):
     ego_vec = [x - mapx[prev_wp], y - mapy[prev_wp], 0]
     map_vec = [n_x, n_y, 0]
     d_cross = np.cross(ego_vec, map_vec)
-    
+
     if d_cross[-1] > 0:
         frenet_d = -frenet_d
 
@@ -318,7 +366,7 @@ def get_cartesian(s, d, mapx, mapy, maps):
     next_wp = np.mod(prev_wp + 1, len(mapx))
 
     dx = (mapx[next_wp] - mapx[prev_wp])
-    dy = (mapy[next_wp] - mapy[prev_wp]) 
+    dy = (mapy[next_wp] - mapy[prev_wp])
 
     heading = np.arctan2(dy, dx)  # [rad]
 
@@ -353,7 +401,6 @@ def next_waypoint(x, y, mapx, mapy):
 
     return closest_wp
 
-
 def get_closest_waypoints(x, y, mapx, mapy):
     min_len = 1e10
     closest_wp = -1
@@ -374,8 +421,6 @@ def get_closest_waypoints(x, y, mapx, mapy):
 
 def get_dist(x, y, _x, _y):
     return np.sqrt((x - _x) ** 2 + (y - _y) ** 2)
-
-
 
 if __name__ == '__main__':
     try:
