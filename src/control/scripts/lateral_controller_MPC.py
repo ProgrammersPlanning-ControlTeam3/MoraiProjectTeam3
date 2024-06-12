b92aa816be7bba31eae2d171d2073edb00777aeb
jjo4342 <jjo4342@gmail.com>
2024-06-12 17:24:00 +0900

    [feat] code refactoring_lateral controller

--- src/control/scripts/lateral_controller_MPC.py
+++ src/control/scripts/lateral_controller_MPC.py
@@ -11,6 +11,8 @@ from morai_msgs.msg import EgoVehicleStatus
 import matplotlib.pyplot as plt
 import cvxpy as cp
 import time
+from controller_utils import *
+
 
 class MPCController:
     def __init__(self):
@@ -186,29 +188,6 @@ class MPCController:
         if self.end_time is None:
             self.end_time = time.time()
 
-def plot_paths(global_path, x_ego, y_ego, total_time, variance, mean_error, max_error):
-    if global_path is None:
-        rospy.logwarn("Global path is None, cannot plot paths.")
-        return
-
-    x_global = [pose.pose.position.x for pose in global_path.poses]
-    y_global = [pose.pose.position.y for pose in global_path.poses]
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(x_global, y_global, 'k--', label='Global Path')
-    plt.plot(x_ego, y_ego, 'b-', label='Ego Vehicle Path')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend(loc="best")
-    plt.title('Vehicle Path Tracking')
-    plt.grid(True)
-
-    plt.text(0.95, 0.05, f'Total Time: {total_time:.2f}s\nVariance: {variance:.4f}\nMean Error: {mean_error:.4f}\nMax Error: {max_error:.4f}', 
-             horizontalalignment='right',
-             verticalalignment='bottom',
-             transform=plt.gca().transAxes)
-
-    plt.show()
 
 if __name__ == "__main__":
     rospy.init_node('mpc_path_tracking_node', anonymous=True)
--- src/control/scripts/lateral_controller_PID.py
+++ src/control/scripts/lateral_controller_PID.py
@@ -10,6 +10,7 @@ from nav_msgs.msg import Odometry, Path
 from morai_msgs.msg import EgoVehicleStatus, ObjectStatusList
 import matplotlib.pyplot as plt
 import time
+from controller_utils import *
 
 
 class pid_feedforward:
@@ -241,28 +242,6 @@ class pid_feedforward:
         if self.end_time is None:
             self.end_time = time.time()
 
-def plot_paths(global_path, x_ego, y_ego, total_time, variance, mean_error, max_error):
-    if global_path is None:
-        return
-
-    x_global = [pose.pose.position.x for pose in global_path.poses]
-    y_global = [pose.pose.position.y for pose in global_path.poses]
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(x_global, y_global, 'k--', label='Global Path')
-    plt.plot(x_ego, y_ego, 'b-', label='Ego Vehicle Path')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.legend(loc="best")
-    plt.title('Vehicle Path Tracking')
-    plt.grid(True)
-
-    plt.text(0.95, 0.05, f'Total Time: {total_time:.2f}s\nVariance: {variance:.4f}\nMean Error: {mean_error:.4f}\nMax Error: {max_error:.4f}', 
-             horizontalalignment='right',
-             verticalalignment='bottom',
-             transform=plt.gca().transAxes)
-
-    plt.show()
 
 # if __name__ == "__main__":
 #     rospy.init_node('path_tracking_node', anonymous=True)
